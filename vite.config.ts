c225bc369b085a828b0b58b1c70908c18e3f0895
Viktor Zhurbin <vzhurbin@gmail.com>
2025-05-02 14:38:19 +0200

    fix tabler icons module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+		},
+	},
 });

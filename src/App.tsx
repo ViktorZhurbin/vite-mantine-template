c2cd26bfcc77dfd73ebdea1f9a1332ac7c35c167
Viktor Zhurbin <vzhurbin@gmail.com>
2025-11-17 18:31:05 +0100

    cleanup biome config

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import { Button, createTheme, MantineProvider, Title } from "@mantine/core";
 import { useState } from "react";
 
 const theme = createTheme({
-	/** Put your mantine theme override here */
+  /** Put your mantine theme override here */
 });
 
 export const App = () => {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<MantineProvider theme={theme}>
-			<Title>Vite + React</Title>
+  return (
+    <MantineProvider theme={theme}>
+      <Title>Vite + React</Title>
 
-			<Button onClick={() => setCount((count) => count + 1)}>
-				count is {count}
-			</Button>
-		</MantineProvider>
-	);
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+    </MantineProvider>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,28 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				advancedChunks: {
-					groups: [
-						{
-							name: "react",
-							test: /node_modules\/(react|react-dom)/,
-						},
-						{
-							name: "ui",
-							test: /node_modules\/(@mantine|@tabler\/icons-react)/,
-						},
-					],
-				},
-			},
-		},
-	},
-	resolve: {
-		alias: {
-			"@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
-		},
-	},
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            {
+              name: "react",
+              test: /node_modules\/(react|react-dom)/,
+            },
+            {
+              name: "ui",
+              test: /node_modules\/(@mantine|@tabler\/icons-react)/,
+            },
+          ],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
 });
